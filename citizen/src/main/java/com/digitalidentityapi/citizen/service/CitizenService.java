fed708188d7e6eda4f392052b6cf2d30b1340d27
Juan Felipe Monsalve Palacio <jumonsalve@sistecredito.com>
2024-02-12 22:09:43 -0500

    consumo servicio getoperators

---
+++ citizen/src/main/java/com/digitalidentityapi/citizen/service/CitizenService.java
@@ -0,0 +1,10 @@
+package com.digitalidentityapi.citizen.service;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class CitizenService {
+
+}
