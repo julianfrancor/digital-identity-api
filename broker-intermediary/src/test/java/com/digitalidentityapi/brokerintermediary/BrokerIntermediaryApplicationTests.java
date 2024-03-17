c72c9524eb16752ae6b288e66b70c3ba99e5f1e6
Julian Franco <julianfrancor@gmail.com>
2024-03-16 22:12:53 -0500

    [FTR] Initial scaffolding for Broker Intermediary microservice

---
+++ broker-intermediary/src/test/java/com/digitalidentityapi/brokerintermediary/BrokerIntermediaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalidentityapi.brokerintermediary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerIntermediaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
