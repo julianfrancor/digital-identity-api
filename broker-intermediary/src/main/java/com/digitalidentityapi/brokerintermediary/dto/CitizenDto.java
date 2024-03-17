ea5640be811e2c088669fd4bd8928fa71d522c69
Julian Franco <julianfrancor@gmail.com>
2024-03-17 18:38:15 -0500

    [FTR] Update dtos

--- broker-intermediary/src/main/java/com/digitalidentityapi/brokerintermediary/dto/CitizenDto.java
+++ broker-intermediary/src/main/java/com/digitalidentityapi/brokerintermediary/dto/CitizenDto.java
@@ -7,28 +7,16 @@ import java.util.Date;
 @Data
 public class CitizenDto {
     private String identification;
-
     private String identificationType;
-
     private String firstName;
-
     private String secondName;
-
     private String lastName;
-
     private String secondLastName;
-
     private String address;
-
     private String email;
-
     private String phone;
-
     private String status;
-
     private Date createdAt;
-
     private Date updatedAt;
-
     private Date deletedAt;
 }
--- broker-intermediary/src/main/java/com/digitalidentityapi/brokerintermediary/dto/TransferDto.java
+++ broker-intermediary/src/main/java/com/digitalidentityapi/brokerintermediary/dto/TransferDto.java
@@ -6,19 +6,8 @@ import java.util.Date;
 
 @Data
 public class TransferDto {
-    private int id;
-
     private String citizenId;
-
     private String citizenEmail;
-
     private String operatorId;
-
     private Date transferDate;
-
-    private Date createdAt;
-
-    private Date updatedAt;
-
-    private Date deletedAt;
 }
