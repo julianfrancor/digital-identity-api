48d50727a1791e643292a0a05fd3e8ca54f3ed9d
Julian Franco <julianfrancor@gmail.com>
2024-03-17 19:28:00 -0500

    [FTR] Update Broker intermediary service

--- broker-intermediary/src/main/java/com/digitalidentityapi/brokerintermediary/service/impl/BrokerIntermediaryServiceImpl.java
+++ broker-intermediary/src/main/java/com/digitalidentityapi/brokerintermediary/service/impl/BrokerIntermediaryServiceImpl.java
@@ -8,6 +8,8 @@ import com.digitalidentityapi.brokerintermediary.dto.DocumentDto;
 import com.digitalidentityapi.brokerintermediary.dto.TransferDto;
 import com.digitalidentityapi.brokerintermediary.producer.RabbitPublishMessage;
 import com.digitalidentityapi.brokerintermediary.service.IBrokerIntermediaryService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,27 +23,61 @@ public class BrokerIntermediaryServiceImpl implements IBrokerIntermediaryService
     @Autowired
     private final RabbitPublishMessage rabbitPublishMessage;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Override
     public void handleCitizenOperations(String operation, CitizenDto citizenDto) {
         CitizenMessage citizenMessage = new CitizenMessage(operation, citizenDto);
+        String citizenMessageString = getCitizenMessageString(citizenMessage);
         System.out.println(citizenMessage.getOperation());
-        rabbitPublishMessage.sendMessageToQueue(CITIZEN_QUEUE, String.valueOf(citizenMessage));
+        rabbitPublishMessage.sendMessageToQueue(CITIZEN_QUEUE, citizenMessageString);
         System.out.println("Message successfully published in Citizen Queue");
+    }
+
+    private String getCitizenMessageString(CitizenMessage citizenMessage) {
+        String message = "";
+        try {
+            message = objectMapper.writeValueAsString(citizenMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return message;
     }
 
     @Override
     public void handleDocumentOperations(String operation, DocumentDto documentDto) {
         DocumentMessage documentMessage = new DocumentMessage(operation, documentDto);
         System.out.println(documentMessage.getOperation());
-        rabbitPublishMessage.sendMessageToQueue(DOCUMENT_QUEUE, String.valueOf(documentMessage));
+        rabbitPublishMessage.sendMessageToQueue(DOCUMENT_QUEUE, getDocumentMessageString(documentMessage));
         System.out.println("Message successfully published in Document Queue");
+    }
+
+    private String getDocumentMessageString(DocumentMessage documentMessage) {
+        String message = "";
+        try {
+            message = objectMapper.writeValueAsString(documentMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return message;
     }
 
     @Override
     public void handleTransferOperations(TransferDto transferDto) {
         TransferMessage transferMessage = new TransferMessage("CREATE", transferDto);
         System.out.println(transferMessage.getOperation());
-        rabbitPublishMessage.sendMessageToQueue(TRANSFERS_QUEUE, String.valueOf(transferMessage));
+        rabbitPublishMessage.sendMessageToQueue(TRANSFERS_QUEUE, getTransferMessageString(transferMessage));
         System.out.println("Message successfully published in Transfers Queue");
+    }
+
+    private String getTransferMessageString(TransferMessage transferMessage) {
+        String message = "";
+        try {
+            message = objectMapper.writeValueAsString(transferMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return message;
     }
 }
