5e6e9487ad7f8bdef53e55074a5c27f625b47f87
monsalvike <monsalvike@gmail.com>
2024-03-19 15:29:53 -0500

    se actualiza Dockerfile

--- operators/src/main/java/com/digitalidentityapi/operators/controller/getOperatorsController.java
+++ operators/src/main/java/com/digitalidentityapi/operators/controller/getOperatorsController.java
@@ -1,17 +1,11 @@
 package com.digitalidentityapi.operators.controller;
 
 import com.digitalidentityapi.operators.service.GetOperatorsServices;
-import com.digitalidentityapi.operators.service.SignDocumentsServices;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/getoperators", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -21,7 +15,7 @@ public class getOperatorsController {
 
     @GetMapping
     public String getOperators() {
-       return getOperatorsServices.getOperators();
+        return getOperatorsServices.getOperators();
     }
 }
 
