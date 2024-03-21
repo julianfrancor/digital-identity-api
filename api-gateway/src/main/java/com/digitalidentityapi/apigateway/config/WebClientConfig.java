c3f96c483d06672abc2a9452d196fc4aa039a956
monsalvike <monsalvike@gmail.com>
2024-03-21 16:55:11 -0500

    Ajustes ApiGateway

--- api-gateway/src/main/java/com/digitalidentityapi/apigateway/config/WebClientConfig.java
+++ api-gateway/src/main/java/com/digitalidentityapi/apigateway/config/WebClientConfig.java
@@ -1,7 +1,13 @@
 package com.digitalidentityapi.apigateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 public class WebClientConfig {
-
+    @Bean
+    public WebClient webClient() {
+        return WebClient.create();
+    }
 }
