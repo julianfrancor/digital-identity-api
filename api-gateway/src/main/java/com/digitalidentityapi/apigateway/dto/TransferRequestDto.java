79c1b625a79b4fa6ea1a159a6c2b6eb3cf122de6
Julian Franco <julianfrancor@gmail.com>
2024-03-21 18:11:03 -0500

    [FTR] Update TransferDto

--- api-gateway/src/main/java/com/digitalidentityapi/apigateway/dto/TransferRequestDto.java
+++ api-gateway/src/main/java/com/digitalidentityapi/apigateway/dto/TransferRequestDto.java
@@ -2,11 +2,11 @@ package com.digitalidentityapi.apigateway.dto;
 
 
 public class TransferRequestDto {
-    private Long citizenIdentification;
+    private String citizenIdentification;
     private String citizenEmail;
     private String destinationOperatorId;
 
-    public TransferRequestDto(Long citizenIdentification, String citizenEmail, String destinationOperatorId) {
+    public TransferRequestDto(String citizenIdentification, String citizenEmail, String destinationOperatorId) {
         this.citizenIdentification = citizenIdentification;
         this.citizenEmail = citizenEmail;
         this.destinationOperatorId = destinationOperatorId;
--- broker-intermediary/src/main/java/com/digitalidentityapi/brokerintermediary/dto/TransferRequestDto.java
+++ broker-intermediary/src/main/java/com/digitalidentityapi/brokerintermediary/dto/TransferRequestDto.java
@@ -6,11 +6,11 @@ import java.util.Date;
 
 @Data
 public class TransferRequestDto {
-    private Long citizenIdentification;
+    private String citizenIdentification;
     private String citizenEmail;
     private String destinationOperatorId;
 
-    public TransferRequestDto(Long citizenIdentification, String citizenEmail, String destinationOperatorId) {
+    public TransferRequestDto(String citizenIdentification, String citizenEmail, String destinationOperatorId) {
         this.citizenIdentification = citizenIdentification;
         this.citizenEmail = citizenEmail;
         this.destinationOperatorId = destinationOperatorId;
